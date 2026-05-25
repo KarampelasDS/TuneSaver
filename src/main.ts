e852bfcee8c5d7424ac158c8f2df93905cb2eac7
Dimitrios-Spiridon Karampelas <24dimitris@gmail.com>
2026-05-25 19:58:45 +0300

    Changed Default Window Size

--- src/main.ts
+++ src/main.ts
@@ -70,8 +70,8 @@ const handleCallback = (url: string) => {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 900,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
@@ -87,7 +87,7 @@ const createWindow = () => {
   }
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // Mac/Linux
